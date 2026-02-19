9d305f6692447361efffd7ed3b906f1c986535d7
dasunfcode <dasun@fcodelabs.com>
2026-02-19 13:42:15 +0530

    project restructured

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,32 +1,32 @@
 import { test, expect } from '@playwright/test';
 import { getOtpFromEmail } from '../utils/emailOtpReader';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 test('TC001_user should login successfully using email OTP', async ({ page }) => {
   test.setTimeout(90000);
 
-  await page.goto('https://qa.msupport.mone.am/login', {
+  await page.goto(`${process.env.BASE_URL}/login`, {
     waitUntil: 'domcontentloaded',
     timeout: 60000,
   });
 
   await expect(page).toHaveTitle('MSupport');
 
-  await page.fill('input[name="email"]', 'dasuntest5@gmail.com');
-  await page.fill('input[name="password"]', 'Ddh@sivalicc*99');
+  await page.fill('input[name="email"]', process.env.EMAIL || '');
+  await page.fill('input[name="password"]', process.env.PASSWORD || '');
 
   await page.getByRole('button', { name: 'Login' }).click();
 
   const otpInput = page.locator('input[data-input-otp="true"]');
   await expect(otpInput).toBeVisible({ timeout: 30000 });
 
-  await page.waitForTimeout(3000);
   const otp = await getOtpFromEmail();
-
   await otpInput.focus();
   await page.keyboard.type(otp.toString());
 
   await expect(page).toHaveURL(/dashboard\/organizations/);
 
-  await page.context().storageState({ path: 'auth.json' });
-
+  await page.context().storageState({ path: process.env.AUTH_JSON_PATH || '' });
 });
--- utils/emailOtpReader.ts
+++ utils/emailOtpReader.ts
@@ -1,11 +1,13 @@
-// utils/emailOtpReader.ts
+import dotenv from 'dotenv';
 const MailListener = require("mail-listener2");
+
+dotenv.config();
 
 export function getOtpFromEmail(): Promise<string> {
   return new Promise((resolve, reject) => {
     const mailListener = new MailListener({
-      username: "dasuntest5@gmail.com",
-      password: "gwjg qkav pzim nmwr",
+      username: process.env.EMAIL,
+      password: process.env.EMAIL_PASSWORD,
       host: "imap.gmail.com",
       port: 993,
       tls: true,
@@ -31,6 +33,6 @@ export function getOtpFromEmail(): Promise<string> {
     setTimeout(() => {
       mailListener.stop();
       reject("OTP email not received within timeout");
-    }, 60000);
+    }, parseInt(process.env.OTP_TIMEOUT || "60000"));
   });
 }
